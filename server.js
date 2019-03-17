d8244a860b7f6ee78a14b89c372255f1731e2893
Cody Rush <codyrush@gmail.com>
2019-03-16 17:22:26 -0700

    updates to server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+// ********************************************************************************
+// Server.js
 var app = require("express");
+
+// Set up Express App
+// ================================================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Set up Express app to handle data parsing
+// ================================================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Starting Express app
+// ================================================================================
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+});
+
+// ********************************************************************************
