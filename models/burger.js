b68090d1c818f6b215502f6b07bccab2e2c88fcf
Cody Rush <codyrush@gmail.com>
2019-03-18 16:25:12 -0700

    updates

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,20 @@
 var orm = require("../config/orm.js");
+
+var cat = {
+    all: function(cb) {
+      orm.all("cats", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("cats", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("cats", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 // ********************************************************************************
 // Server.js
 var express = require("express");
+var exphbs = require("express-handlebars");
 
 // Set up Express App
 // ================================================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Set up Express app to handle data parsing
 // ================================================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
 // Static directory
 // ================================================================================
 app.use(express.static("public"));
-
-// Routes
-// ================================================================================
-
 
 // Starting Express app
 // ================================================================================
